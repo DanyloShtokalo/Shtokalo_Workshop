7078c8c47464395fe1ec0bd0083fe9bb9dac42c9
Danylo Shtokalo <shtokalo.danylo.lviv@gmail.com>
2021-10-23 16:23:46 +0300

    corrected version of Workshop 2 using interrupts

---
+++ Shtokalo_Workshop_2_Interrupts/Core/Inc/extern_variables.h
@@ -0,0 +1,3 @@
+unsigned int ledStatus;			//Status-code that indicates which LED should be on
+								//Value is from 0 to 3 (since we have 4 LED`s)
+unsigned int ledFrequency;		//Frequency of blinking in miliseconds of delay time
